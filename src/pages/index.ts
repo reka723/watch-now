4268d95060cfece9b09e8616bcb113642a7a4976
Reka Dobany <rdobany@amelco.co.uk>
2025-02-21 19:38:39 +0100

    pages import

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./Home"
+export { default as LibraryPage } from "./Library"
+export { default as SearchPage } from "./Search"
+export { default as LandingPage } from "./Landing"
